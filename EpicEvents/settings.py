f3eb8dd9a3f86a626692f499e59e7941452cd96c
Adrien Burgun <adrienburgun@hotmail.fr>
2022-05-23 09:35:37 +0900

    Removed unused auth classes

--- EpicEvents/settings.py
+++ EpicEvents/settings.py
@@ -136,8 +136,6 @@ AUTH_USER_MODEL = "authentication.User"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
